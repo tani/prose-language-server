f002952962414bcf88bffd5f2257bb1a0180b2b5
TANIGUCHI Masaya <xtaniguchimasaya@gmail.com>
2018-10-15 21:45:50 +0900

    Add ignore property for global installation

--- cli.js
+++ cli.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 require("@babel/register")({
+    ignore: [],
     extensions: ".ts",
     presets: [["@babel/preset-env", { targets: { node: "current" } }], "@babel/preset-typescript"]
 });
