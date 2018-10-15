30543b967b3470f22e92ca4283a21c194aafc953
TANIGUCHI Masaya <xtaniguchimasaya@gmail.com>
2018-10-15 22:22:58 +0900

    Remove babel

--- cli.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-require("@babel/register")({
-    ignore: [],
-    extensions: ".ts",
-    presets: [["@babel/preset-env", { targets: { node: "current" } }], "@babel/preset-typescript"]
-});
-require("./index.ts");
--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-import { createConnection, TextDocuments, TextDocument, TextDocumentChangeEvent, Diagnostic, DiagnosticSeverity, ProposedFeatures } from "vscode-languageserver";
-import { exec, spawn } from "child_process";
-import fetch from "node-fetch";
-import qs from "qs";
+#!/usr/bin/env node
+const { createConnection, TextDocuments, TextDocument, TextDocumentChangeEvent, Diagnostic, DiagnosticSeverity, ProposedFeatures } = require("vscode-languageserver");
+const { exec, spawn } = require("child_process");
+const fetch = require("node-fetch");
+const qs = require("qs");
 const getPort = require("get-port")
 const fileUrlToPath = require("file-uri-to-path");
 const writeGood = require("write-good");
-let languageToolClient = async (text: string) => ([] as any[]);
+let languageToolClient = async (text) => ([]);
 
 const languageToolServer = {
-    listen (port: number) {
+    listen (port) {
 	const server = spawn("java", ["-cp", `${__dirname}/LanguageTool-4.3/languagetool-server.jar`, "org.languagetool.server.HTTPServer", "--port", `${port}`]);
 	server.stdout.on("data", (data)=>{
 	    console.warn(data.toString());
 	    if(data.toString().match(/Server started/)) {
-		languageToolClient = async (text: string) => {
+		languageToolClient = async (text) => {
 		    const language = 'en-US';
 		    const response = await fetch(`http://localhost:${port}/v2/check?${qs.stringify({language, text})}`);
 		    const json = await response.json();
@@ -38,7 +39,7 @@ connection.onInitialize(() => ({
     }
 }));
 
-const removeMarkdown = (markdown: string) => {
+const removeMarkdown = (markdown) => {
     return markdown
 	.replace(/```[\s\S]*?```/mg, (s) => " ".repeat(s.length))
     	.replace(/~~~[\s\S]*?~~~/mg, (s) => " ".repeat(s.length))
@@ -48,32 +49,32 @@ const removeMarkdown = (markdown: string) => {
 	.replace(/#*/mg, (s)=>" ".repeat(s.length))
 }
 
-const validateTextDocumentChange = async (change: TextDocumentChangeEvent) => {
+const validateTextDocumentChange = async (change) => {
     const text = process.argv.indexOf("--markdown") > 0 ? removeMarkdown(change.document.getText()) : change.document.getText();
     const writeGoodWarnings = await writeGood(text);
     const languageToolErrors = await languageToolClient(text);
-    const diagnostics: Diagnostic[] = ([] as Diagnostic[])
-	.concat(writeGoodWarnings.map((warning: any)=>({
-	    severity: DiagnosticSeverity.Hint,
-	    code: text.slice(warning.index, warning.index+warning.offset),
-	    range: {
-		start: change.document.positionAt(warning.index),
-		end: change.document.positionAt(warning.index + warning.offset)
-	    },
-	    message: warning.reason,
-	    srouce: "write-good"
-	})), languageToolErrors.filter((error: any) => {
-	    return !error.message.match(/repeated a whitespace/)
-	}).map((error: any)=>({
-	    severity: DiagnosticSeverity.Error,
-	    code: text.slice(error.offset, error.offset+error.length),
-	    range: {
+    const diagnostics = ([])
+	  .concat(writeGoodWarnings.map((warning)=>({
+	      severity: DiagnosticSeverity.Hint,
+	      code: text.slice(warning.index, warning.index+warning.offset),
+	      range: {
+		  start: change.document.positionAt(warning.index),
+		  end: change.document.positionAt(warning.index + warning.offset)
+	      },
+	      message: warning.reason,
+	      srouce: "write-good"
+	  })), languageToolErrors.filter((error) => {
+	      return !error.message.match(/repeated a whitespace/)
+	  }).map((error)=>({
+	      severity: DiagnosticSeverity.Error,
+	      code: text.slice(error.offset, error.offset+error.length),
+	      range: {
 		start: change.document.positionAt(error.offset),
 		end: change.document.positionAt(error.offset+error.length)
 	    },
 	    message: error.message,
 	    source: "languagetool"
-	})));
+	  })));
     connection.sendDiagnostics({ uri: change.document.uri, diagnostics });
 }
 
